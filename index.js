d77f23039dfed165c647916329949c5fd1398d9b
boredasfawk <olonnyetaylor@gmail.com>
2020-03-04 18:25:43 -0800

    test

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ mongoose
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With").build();
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
