1d3841ec539e41534f214fcf946cc8e3e3d5c7d6
boredasfawk <olonnyetaylor@gmail.com>
2020-03-04 18:19:02 -0800

    test

--- index.js
+++ index.js
@@ -28,9 +28,8 @@ const premisesLoginvalidation = require(premisesLogValPath);
 
 // Allow inbound traffic from origin URL
 const corsOptions = {
-  origin: 'https://test.cyberworks.tech' || 'https://zealous-wiles-7601ce.netlify.com',
+  origin: '*',
   credentials: true,
-  enablePreflight: true,
   optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
@@ -115,6 +114,7 @@ app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
