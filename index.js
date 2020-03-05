84f34a8e9bf2e934b0ee6ba8315fd6bc113a0262
boredasfawk <olonnyetaylor@gmail.com>
2020-03-04 17:57:52 -0800

    test

--- index.js
+++ index.js
@@ -111,9 +111,10 @@ mongoose
 /*ROUTES START*/
 
 // headers for cors
-app.all(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
